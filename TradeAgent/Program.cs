48cd13722ad607b7c1e5c2a159cd74678a50ce64
sculove <sculove@gmail.com>
2014-07-10 13:29:34 +0900

    환경 점�

--- TradeAgent/Program.cs
+++ TradeAgent/Program.cs
@@ -3,24 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using log4net;
 
 namespace TradeAgent
 {
     static class Program
     {
+        private static readonly ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// 
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
+            
             log.Info("-d-ds-s-ds-d-sd-sd-sd-ds");
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TradeAgentForm());
+
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new TradeAgentForm());
         }
     }
 }
